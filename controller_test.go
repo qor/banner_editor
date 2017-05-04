9092a6aa64ef86a4f3b0e6cb37d3c71040c4c622
binku <binku87@gmail.com>
2017-05-04 18:14:33 +0800

    add new page test

---
+++ controller_test.go
@@ -0,0 +1,87 @@
+package banner_editor
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/fatih/color"
+	"github.com/qor/admin"
+	"github.com/qor/qor"
+	"github.com/qor/qor/test/utils"
+)
+
+var (
+	mux    = http.NewServeMux()
+	Server = httptest.NewServer(mux)
+	db     = utils.TestDB()
+	Admin  = admin.New(&qor.Config{DB: db})
+)
+
+func init() {
+
+	// Banner Editor
+	type bannerEditorArgument struct {
+		Value string
+	}
+	type subHeaderSetting struct {
+		Text  string
+		Color string
+	}
+	type buttonSetting struct {
+		Text string
+		Link string
+	}
+	subHeaderRes := Admin.NewResource(&subHeaderSetting{})
+	subHeaderRes.Meta(&admin.Meta{Name: "Text"})
+	subHeaderRes.Meta(&admin.Meta{Name: "Color"})
+
+	buttonRes := Admin.NewResource(&buttonSetting{})
+	buttonRes.Meta(&admin.Meta{Name: "Text"})
+	buttonRes.Meta(&admin.Meta{Name: "Link"})
+
+	RegisterElement(&Element{
+		Name:     "Sub Header",
+		Template: "<em style=\"color: {{Color}};\">{{Text}}</em>",
+		Resource: subHeaderRes,
+		Context: func(c *Context, r interface{}) *Context {
+			return c
+		},
+	})
+	RegisterElement(&Element{
+		Name:     "Button",
+		Template: "<a href='{{Link}}'>{{Text}}</a>",
+		Resource: buttonRes,
+		Context: func(c *Context, r interface{}) *Context {
+			return c
+		},
+	})
+	bannerEditorResource := Admin.AddResource(&bannerEditorArgument{})
+	bannerEditorResource.Meta(&admin.Meta{Name: "Value", Config: &BannerEditorConfig{}})
+
+	if err := db.DropTableIfExists(&QorBannerEditorSetting{}).Error; err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&QorBannerEditorSetting{})
+	Admin.MountTo("/admin", mux)
+}
+
+func TestControllerCRUD(t *testing.T) {
+	resp, _ := http.Get(Server.URL + "/admin/qor_banner_editor_settings/new?kind=Sub%20Header")
+	assetPageHaveAttributes(t, resp, "Text", "Color")
+
+	resp, _ = http.Get(Server.URL + "/admin/qor_banner_editor_settings/new?kind=Button")
+	assetPageHaveAttributes(t, resp, "Text", "Link")
+}
+
+func assetPageHaveAttributes(t *testing.T, resp *http.Response, attributes ...string) {
+	body, _ := ioutil.ReadAll(resp.Body)
+	for _, attr := range attributes {
+		if !strings.Contains(string(body), fmt.Sprintf("QorResource.SerializableMeta.%v", attr)) {
+			t.Error(color.RedString("PageHaveAttrributes: expect page have attributes %v, but got %v", attr, string(body)))
+		}
+	}
+}
