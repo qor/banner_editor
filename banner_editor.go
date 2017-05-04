172802b7bb9500845498c8326bd7f975a6464de4
binku <binku87@gmail.com>
2017-05-04 12:00:18 +0800

    new api and implement

--- banner_editor.go
+++ banner_editor.go
@@ -1,64 +1,82 @@
 package banner_editor
 
 import (
-	"encoding/json"
 	"errors"
-	"strings"
+	"fmt"
+	"html/template"
 
+	"github.com/jinzhu/gorm"
 	"github.com/qor/admin"
 	"github.com/qor/qor/resource"
+	"github.com/qor/serializable_meta"
 )
 
+var (
+	registeredElements []*Element
+)
+
+type Context struct {
+	DB       *gorm.DB
+	Options  map[string]interface{}
+	FuncMaps template.FuncMap
+}
+
 type BannerEditorConfig struct {
-	Width    int64
-	Height   int64
-	Elements []*Element
-	Fixed    bool
+	Elements        []string
+	SettingResource *admin.Resource
+}
+
+type QorBannerEditorSetting struct {
+	gorm.Model
+	serializable_meta.SerializableMeta
 }
 
 type Element struct {
 	Name     string
 	Template string
 	Resource *admin.Resource
+	Context  func(context *Context, setting interface{}) *Context
 }
 
 func init() {
 	admin.RegisterViewPath("github.com/qor/banner_editor/views")
 }
 
-func (bannerEditorConfig *BannerEditorConfig) ConfigureQorMeta(metaor resource.Metaor) {
+func RegisterElement(e *Element) {
+	registeredElements = append(registeredElements, e)
+}
+
+func (config *BannerEditorConfig) ConfigureQorMeta(metaor resource.Metaor) {
 	if meta, ok := metaor.(*admin.Meta); ok {
 		meta.Type = "banner_editor"
 		Admin := meta.GetBaseResource().(*admin.Resource).GetAdmin()
-		Admin.RegisterFuncMap("banner_editor_configure", func() string {
-			config := meta.Config.(*BannerEditorConfig)
-			configJSON := map[string]interface{}{}
-			elementsJSON := map[string]interface{}{}
-			for _, element := range config.Elements {
-				elementSetting := map[string]interface{}{}
-				elementSetting["title"] = element.Name
-				elementSetting["template"] = element.Template
-				metasSetting := map[string]interface{}{}
-				for _, meta := range element.Resource.Metas {
-					metasSetting[meta.Name] = map[string]string{
-						"default": "aaa",
-						"type":    meta.Type,
-					}
-				}
-				elementSetting["keys"] = metasSetting
-				elementsJSON[strings.Replace(element.Name, " ", "_", -1)] = elementSetting
-			}
 
-			configJSON["width"] = config.Width
-			configJSON["height"] = config.Height
-			configJSON["elements"] = elementsJSON
-			configJSON["fixed"] = config.Fixed
-			jsonString, _ := json.Marshal(configJSON)
-			return string(jsonString)
-		})
+		if config.SettingResource == nil {
+			config.SettingResource = Admin.AddResource(&QorBannerEditorSetting{})
+		}
+
+		router := Admin.GetRouter()
+		router.Get(fmt.Sprintf("%v/new", config.SettingResource.ToParam()), New)
 	}
 }
 
 func (bannerEditorConfig *BannerEditorConfig) GetTemplate(context *admin.Context, metaType string) ([]byte, error) {
 	return nil, errors.New("not implemented")
+}
+
+func GetElement(name string) *Element {
+	for _, e := range registeredElements {
+		if e.Name == name {
+			return e
+		}
+	}
+	return nil
+}
+
+func (setting QorBannerEditorSetting) GetSerializableArgumentResource() *admin.Resource {
+	element := GetElement(setting.Kind)
+	if element != nil {
+		return element.Resource
+	}
+	return nil
 }
