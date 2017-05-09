705829755b03efe96668e63da9961408c4a219cd
jason weng <wengjianm@gmail.com>
2017-05-09 14:08:28 +0800

    add banner edit drag event @1

--- views/themes/banner_editor/assets/javascripts/banner_editor/banner_editor.js
+++ views/themes/banner_editor/assets/javascripts/banner_editor/banner_editor.js
@@ -17,6 +17,13 @@
         EVENT_ENABLE = 'enable.' + NAMESPACE,
         EVENT_DISABLE = 'disable.' + NAMESPACE,
         EVENT_CLICK = 'click.' + NAMESPACE,
+        EVENT_DRAGSTART = 'dragstart.' + NAMESPACE,
+        EVENT_DRAGEND = 'dragend.' + NAMESPACE,
+        EVENT_DROP = 'drop.' + NAMESPACE,
+
+
+
+        CLASS_DRAGGABLE = '[draggable]',
         CLASS_TOOLBAR_BUTTON = '.qor-bannereditor__button',
         CLASS_BANNEREDITOR_VALUE = '.qor-bannereditor__value',
         CLASS_CANVAS = '.qor-bannereditor__canvas';
@@ -39,15 +46,43 @@
         },
 
         bind: function() {
-            this.$element.on(EVENT_CLICK, CLASS_TOOLBAR_BUTTON, this.addElements.bind(this));
+            this.$element
+                .on(EVENT_CLICK, CLASS_TOOLBAR_BUTTON, this.addElements.bind(this))
+                .on(EVENT_DRAGSTART, CLASS_DRAGGABLE, this.handleDragStart.bind(this))
+                .on(EVENT_DRAGEND, CLASS_DRAGGABLE, this.handleDragEnd.bind(this))
+                .on(EVENT_DROP, CLASS_DRAGGABLE, this.handleDrop.bind(this));
+
             $(document).on(EVENT_CLICK, '.qor-bannereditor__content button[type="submit"]', this.renderElement.bind(this));
         },
 
         initToolbar: function() {
             let $toolbar = $(window.Mustache.render(QorBannerEditor.toolbar, this.config));
 
-            $toolbar.appendTo($('.qor-bannereditor__toolbar'));
+            $toolbar.appendTo($('.qor-bannereditor__toolbar-btns'));
             this.$popover = $(QorBannerEditor.popover).appendTo('body');
+        },
+
+        handleDragStart: function(e) {
+            // console.log(e)
+            e.originalEvent.dataTransfer.setData('text/plain', null); // required otherwise doesn't work
+        },
+
+        handleDragEnd: function(e) {
+            console.log(e)
+
+            $(e.target).css({
+                'left': e.originalEvent.offsetX,
+                'top': e.originalEvent.offsetY
+            })
+        },
+
+        handleDrop: function(e) {
+            // if (e.stopPropagation) {
+            //     e.stopPropagation(); // stops the browser from redirecting.
+            // }
+            // console.log(e)
+
+            return false;
         },
 
         renderElement: function(e) {
@@ -71,7 +106,7 @@
                 success: function(data) {
                     // console.log(data);
 
-                    $canvas.append(data.Template);
+                    $canvas.append($(data.Template).attr('draggable', true));
                     $popover.qorModal('hide');
                 }
             });
@@ -103,7 +138,7 @@
         }
     };
 
-    QorBannerEditor.toolbar = `[[#toolbar]]<li><button class="qor-bannereditor__button" data-banner-url="[[CreateUrl]]" data-title="[[Name]]" type="button">[[Name]]</button></li>[[/toolbar]]`;
+    QorBannerEditor.toolbar = `[[#toolbar]]<button class="mdl-button mdl-button--colored mdl-js-button qor-bannereditor__button" data-banner-url="[[CreateUrl]]" data-title="[[Name]]" type="button">[[Name]]</button>[[/toolbar]]`;
 
     QorBannerEditor.popover = `<div class="qor-modal fade qor-bannereditor__form" tabindex="-1" role="dialog" aria-hidden="true">
                                   <div class="mdl-card mdl-shadow--2dp" role="document">
